b9814240e1f255baaada05bdbe1d77f2e3a22d02
Molnár-Zékány Szabina <szabina.zekany@bayzoltan.hu>
2023-03-28 15:02:02 +0200

    Upload

---
+++ ListvsArrayList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+
+namespace ListvsArrayList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arraylist = new ArrayList();
+
+            arraylist.Add(1);
+            arraylist.Add(2);
+            arraylist.Add("three");
+
+            int sum = 0;
+               
+            foreach(int i in arraylist)
+            {
+                sum += i;
+            }
+
+            List<int> list = new List<int>();
+
+            list.Add(1);
+            list.Add(2);
+            //list.Add("three");
+        }
+    }
+}
